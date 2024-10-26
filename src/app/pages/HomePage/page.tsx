c66c61319838982d60c7fb4c857d3a4f54def7bb
Maqddus-Askhbn <maqddusaskhab@gmail.com>
2024-10-26 21:27:57 +0500

    updated

--- src/app/pages/HomePage/page.tsx
+++ src/app/pages/HomePage/page.tsx
@@ -1,10 +1,10 @@
-import HeroSlider from '../../../../components/HeroSection'
-import Navbar from '../../../../components/NavBar'
-import IndustrySection from '../../../../components/IntroSection'
+import HeroSlider from '../../../components/HeroSection'
+// import Navbar from '../../../components/NavBar'
+import IndustrySection from '../../../components/IntroSection'
 import React from 'react'
-import TeamSection from '../../../../components/TeamSection'
-import Testimonials from '../../../../components/Testimonials'
-import Footer from '../../../../components/Footer'
+import TeamSection from '../../../components/TeamSection'
+import Testimonials from '../../../components/Testimonials'
+// import Footer from '../../../../components/Footer'
 
 function Homepage() {
   return (
